0401e9e950ecb0bbee1326e2aceeeb57170adb2b
Raf128 <dbkickva@outlook.com>
2024-05-20 13:09:59 -0400

    Update index.js

--- things/index.js
+++ things/index.js
@@ -1,5 +1,6 @@
 function start() {
   document.location.href = /os.html/;
+  if 
 }
 function dark() {
    document.getElementById("body").classList.remove('background');
@@ -7,14 +8,18 @@ function dark() {
   document.getElementById("body").classList.remove('light');
    document.getElementById("body").classList.add('dark');
    document.getElementById("header").classList.add('dark');
-
+  let dark = true;
+  let light = false;
+  let normal = false;
 }
 function light() {
   document.getElementById("body").classList.remove('dark');
   document.getElementById("body").classList.remove('light');
   document.getElementById("body").classList.add('background');
   document.getElementById("header").classList.remove('dark');
-
+  let dark = false;
+  let normal = true;
+  let light = false;
 }
 function bright() {
   document.getElementById("body").classList.remove('dark');
@@ -22,5 +27,7 @@ function bright() {
   document.getElementById("body").classList.add('light');
   document.getElementById("header").classList.remove('dark');
   document.getElementById("body").classList.add('light');
- 
+ let dark = false;
+ let normal = false;
+ let light = true;
 }
