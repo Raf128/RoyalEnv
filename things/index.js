64b733f41b2077086adebb3f03dfc004f31e8bc4
Raf128 <dbkickva@outlook.com>
2024-05-20 11:54:33 -0400

    Update index.js

--- things/index.js
+++ things/index.js
@@ -7,14 +7,14 @@ function dark() {
   document.getElementById("body").classList.remove('light');
    document.getElementById("body").classList.add('dark');
    document.getElementById("header").classList.add('dark');
-  document.getElementById("continue").classList.add('darkb');
+
 }
 function light() {
   document.getElementById("body").classList.remove('dark');
   document.getElementById("body").classList.remove('light');
   document.getElementById("body").classList.add('background');
   document.getElementById("header").classList.remove('dark');
-  document.getElementById("continue").classList.remove('darkb');
+
 }
 function bright() {
   document.getElementById("body").classList.remove('dark');
@@ -22,5 +22,5 @@ function bright() {
   document.getElementById("body").classList.add('light');
   document.getElementById("header").classList.remove('dark');
   document.getElementById("body").classList.add('light');
-  document.getElementById("continue").classList.remove('darkb');
+ 
 }
