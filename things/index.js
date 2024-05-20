892396c987ef480f40788985ec6c5fa8b7f3459a
Raf128 <dbkickva@outlook.com>
2024-05-20 10:52:48 -0400

    Update index.js

--- things/index.js
+++ things/index.js
@@ -3,11 +3,14 @@ function start() {
 }
 function dark() {
    document.getElementById("body").classList.remove('background');
+  document.getElementById("body").classList.remove('dark');
+  document.getElementById("body").classList.remove('light');
    document.getElementById("body").classList.add('dark');
    document.getElementById("header").classList.add('dark');
 }
 function light() {
   document.getElementById("body").classList.remove('dark');
+  document.getElementById("body").classList.remove('light');
   document.getElementById("body").classList.add('background');
   document.getElementById("header").classList.remove('dark');
 }
